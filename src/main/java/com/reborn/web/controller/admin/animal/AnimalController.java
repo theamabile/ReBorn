21152e65d1a53f7c5f7bdefe1d5c8e51368a9a12
JeongHyeJIn <theambabile@gmail.com>
2021-02-01 17:00:48 +0900

    [혜진] 관리자 HTML/CSS, Tiles 추가

--- src/main/java/com/reborn/web/controller/admin/animal/AnimalController.java
+++ src/main/java/com/reborn/web/controller/admin/animal/AnimalController.java
@@ -37,46 +37,12 @@ import com.reborn.web.service.animal.AnimalService;
 @Controller("adminAnimalController")
 @RequestMapping("/admin/animal/")
 public class AnimalController {
-	
-	@Autowired
-	AnimalService animalService;
-
-	@Autowired
-	AnimalKindService kindService;
-
-	@Value("${animal.kindApiUrl}")
-	private String kindUrl;
-	
-	@Value("${animal.apiKey}")
-	private String serviceKey;
 		
 	
 	@RequestMapping("list")
 	public String list() {
-		
-		
-		return "admin.animal.detail";
+		return "admin.animal.list";
 	}
 	
-	
-	// 관리자 페이지에서 목록을 api의 내용으로 업데이트 하겠다는 내용
-	@RequestMapping("update")
-	public String updateList() {
-		
-		animalService.updateListFromAPI(1, 3000, "", "", null, null, "");
-		 
-		return "admin.animal.detail";
-	}
-	
-	
-	// 관리자 축종/품종 업데이트용 함수 : api로부터 읽어와서 값을 insert/update 함
-	@RequestMapping("update/kind")
-	public String kind() {
-
-		kindService.updateKindListFromAPI();
-		
-		return "home.animal.list";
-	}
-		
 	
 }
---
+++ src/main/webapp/WEB-INF/view/admin/inc/footer.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<footer class="footer">
+    한국동물콘텐츠개발협회 <br>
+    대표이사 : 최은희 사업자등록번호 : 197-06-18554 사업자 정보확인 통신판매업 신고 :
+	2020-서울특별시-1102 <br> 사업장주소 : 서울 종로구 인사동길 12 대일빌딩 7층, 15층 대표번호 :
+	010-1111-2222 제품/서비스/멤버십: 2222-3333 구매문의 : 4444-5555
+
+	<br> 본 사이트의 컨텐츠는 저작권법의 보호를 받는 바 무단 전재, 복사, 배포 등을 금합니다. 게시된 이메일
+	주소가 자동 수집되는 것을 거부하며, 이를 위반 시 정보통신법에 의해 처벌됨을 유념하시기 바랍니다.<br>
+	Copyright © 2021 KACDL. All Rights Reserved.
+</footer>    
+</body>
+</html>
